da7e0aba111833363f79964a4927c2e22b78c624
Gustavo Riedo <gh03012@gmail.com>
2021-11-20 12:59:10 -0300

    feat: pequenas alterações pontuais no codigo

--- src/server.ts
+++ src/server.ts
@@ -25,4 +25,4 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
   })
 })
 
-app.listen(3333, () => console.log('Server started!'))
+app.listen(3333, () => console.log('API Iniciada v1.2.1 || Capy Soluções Digitais'))
